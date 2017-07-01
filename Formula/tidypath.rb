88bf32a1411ae1406daaceec2ec1f5c983c2cacf
Joshua Neuheisel <joshua@neuheisel.us>
2017-07-01 18:01:10 -0400

    updated to version 0.1.3

--- Formula/tidypath.rb
+++ Formula/tidypath.rb
@@ -1,8 +1,8 @@
 class Tidypath < Formula
   desc "Tidy up PATH and PATH-like strings."
   homepage "https://github.com/jneu/tidypath"
-  url "https://github.com/jneu/tidypath/releases/download/v0.1.2/tidypath-0.1.2.tar.gz"
-  sha256 "4b7521b267b63037db936a87b082b60736ca9fde498f1861fcfd557d7ff169e2"
+  url "https://github.com/jneu/tidypath/releases/download/v0.1.3/tidypath-0.1.3.tar.gz"
+  sha256 "6a667d1abeb60d1df315bf432115d4c92bb1ba342b8cde225e267f96d6def689"
 
   def install
     system "./configure", "--disable-dependency-tracking",
